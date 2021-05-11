b21a51031d9234bef951e1c105e4e0cae8810f6b
xfilteredwaterx <shaina.milde1@gmail.com>
2021-05-11 15:42:45 +0200

    Stadt, mehr Wald, Movement und BUBBLES!

--- CCProjekt/Assets/Scripts/PlayerController.cs
+++ CCProjekt/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 3;
-
-    private CharacterController cc;
+    public float speed;
     public Animator anim;
+    public GameObject bubbleParticles;
+
+    private Rigidbody playerRb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        cc = GetComponent<CharacterController>();
+        playerRb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Character Movement
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        cc.SimpleMove((transform.forward * verticalInput + transform.right * horizontalInput) * speed);
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        { 
-            anim.SetFloat("Speed_f", 1);
-        }
-        else
+
+        Vector3 movement = (transform.forward * verticalInput + transform.right * horizontalInput);
+
+
+        playerRb.velocity = new Vector3(movement.x * speed, movement.y, movement.z * speed);
+
+        if (Input.GetMouseButtonDown(2))
         {
-            anim.SetFloat("Speed_f", 0);
+            Instantiate(bubbleParticles, transform.position, bubbleParticles.transform.rotation);
         }
     }
 }
