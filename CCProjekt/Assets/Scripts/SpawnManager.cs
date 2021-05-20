6a1930de283f00f4d65814d0bc19d33c690711fb
xfilteredwaterx <shaina.milde1@gmail.com>
2021-05-20 10:48:27 +0200

    Spawn Seeds

--- CCProjekt/Assets/Scripts/SpawnManager.cs
+++ CCProjekt/Assets/Scripts/SpawnManager.cs
@@ -9,10 +9,15 @@ public class SpawnManager : MonoBehaviour
     public Transform[] spawnpoints;
     public DayNightCycler dnc;
 
+    public float boxRadius;
+    public float zRange = 5;
+    public GameObject seedPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnRat", 0f, 3f);
+        InvokeRepeating("SpawnSeeds", 0f, 50f);
     }
 
     // Update is called once per frame
@@ -28,6 +33,14 @@ public class SpawnManager : MonoBehaviour
             int randomPos = Random.Range(0, spawnpoints.Length);
 
             Instantiate(ratPrefab, spawnpoints[randomPos].position, ratPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnSeeds ()
+    {
+        if (!Physics.CheckSphere(transform.position, boxRadius))
+        {
+            Instantiate(seedPrefab, new Vector3(Random.Range(-24, -32), 15.9f, Random.Range(5, -30)), seedPrefab.transform.rotation);
         }
     }
 }
